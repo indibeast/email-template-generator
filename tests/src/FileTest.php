35f0cf4d5ab1c4acd97c2db9cfb354eeb92bae87
indika <indika@indikas-MacBook-Pro.local>
2015-09-21 17:17:24 +0530

    test fix

--- tests/src/FileTest.php
+++ tests/src/FileTest.php
@@ -9,10 +9,10 @@ class FileTest extends \PHPUnit_Framework_TestCase {
 
     public function test_file_writer()
     {
-        $filewriter = new Writer(__DIR__ . '/../files/');
+        $filewriter = new Writer(__DIR__ . '/../files');
         $content = 'Hi {{$name}}';
         $filewriter->setFilename('sample')->setContent($content)->save();
-        $this->assertEquals(__DIR__ . '/../files/',$filewriter->getFilePath());
+        $this->assertEquals(__DIR__ . '/../files',$filewriter->getFilePath());
         $this->assertEquals($content,$filewriter->getContent());
         $this->assertEquals(__DIR__ . '/../files/sample.blade.php',$filewriter->getFileName());
         $this->assertFileExists(__DIR__ . '/../files/sample.blade.php');
